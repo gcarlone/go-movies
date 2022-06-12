fe2ecf8ceb4a235e57ac87d847e024db6b161b09
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2022-06-12 09:52:47 +0200

    added response envelop and pretty print

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -8,6 +9,9 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 )
+
+// An enveloper type to wrap response data
+type envelope map[string]any
 
 // Retrieve the "id" URL parameter from the current request context, then convert it to
 // an integer and return it. If the operation isn't successful, return 0 and an error.
@@ -25,11 +29,21 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 // Define a writeJSON() helper for sending responses. This takes the destination
 // http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
 // header map containing any additional HTTP headers we want to include in the response.
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
 	js, err := json.Marshal(data)
 	if err != nil {
 		return err
+	}
+
+	if app.config.env == "development" {
+		buffer := new(bytes.Buffer)
+		err = json.Indent(buffer, js, "", "  ")
+		if err != nil {
+			js = buffer.Bytes()
+		} else {
+			app.logger.Print("Indentation Error:", err)
+		}
 	}
 
 	// Append a newline to make it easier to view in terminal applications.
